68d1254f3cdea36bd4582f1dd11de52d61185541
Kathy-kay <adigunkafy27@gmail.com>
2023-10-13 17:09:24 +0100

    added custom colors and fonts

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+        Lato: ['Lato', 'sans-serif'],
+        AtypTest: ['AtypTest'],
+      },
+      colors: {
+        black: '#1f1d1d',
+        'nexa-yellow-800': '#ff9a2b',
+        'nexa-yellow-500': 'ffb930',
+        'nexa-yellow-200': '#ffdc73',
+      },
+      screens: {
+        wide: '1440px',
+      },
+    },
   },
   plugins: [],
 };
