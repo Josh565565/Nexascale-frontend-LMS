5f99c9c8f249497ccfdf46f36b3827b93d9092a2
talentlessDeveloper <kareemope52@gmail.com>
2023-10-19 10:51:11 +0100

    eslint fixes

--- Frontend/src/pages/landing/HowitWorks.tsx
+++ Frontend/src/pages/landing/HowitWorks.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable react/jsx-props-no-spreading */
 import { howItWork } from '@/constant';
 import howItWorks from '../../assets/images/howItWorks.png';
 
@@ -7,18 +5,25 @@ const HowitWorks = () => {
   return (
     <div className="flex w-full flex-wrap gap-28 max-lg:flex-col">
       <div className="flex justify-center">
-      <img src={howItWorks} alt="how it works" width={450} height={600} className="object-contain justify-center"/>
+        <img
+          src={howItWorks}
+          alt="how it works"
+          width={450}
+          height={600}
+          className="justify-center object-contain"
+        />
       </div>
-      <div > 
+      <div>
         {howItWork.map((item) => (
           <div
-          className="flex  flex-col justify-center items-center w-full mb-5"
+            className="mb-5  flex w-full flex-col items-center justify-center"
             key={item.label}
           >
             <img src={item.imgURL} alt={item.label} width={50} height={70} />
-            <h3 className='text-center capitalize text-3xl font-normal'>{item.label}</h3>
-            <p className='text-center max-w-md mt-4'>{item.subtext}</p>
-            
+            <h3 className="text-center text-3xl font-normal capitalize">
+              {item.label}
+            </h3>
+            <p className="mt-4 max-w-md text-center">{item.subtext}</p>
           </div>
         ))}
       </div>
